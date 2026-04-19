b1049eb68524a4b24a764298cf380d2bacf59433
Mikhail Ivanov <shirokopuh@gmail.com>
2026-04-19 18:05:16 +0700

    feat(app router): add draft of app routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,24 @@
-import { ConstructorPage, Feed } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
-import { AppHeader } from '@components';
-import { Preloader } from '@ui';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { ProtectedRoute } from '../protected-route';
+import { AppHeader, Modal, OrderInfo } from '@components';
 
 const App = () => {
   /** TODO: взять переменные из стора */
   const error = null;
-
+  const navigate = useNavigate();
   return (
     <div className={styles.app}>
       <AppHeader />
@@ -18,7 +27,46 @@ const App = () => {
           {error}
         </div>
       ) : (
-        <ConstructorPage />
+        <>
+          <Routes>
+            <Route path='*' element={<NotFound404 />} />
+            <Route path='/'>
+              <Route index element={<ConstructorPage />} />
+              <Route path='feed' element={<Feed />} />
+              <Route path='profile' element={<ProtectedRoute />}>
+                <Route index element={<Profile />} />
+                <Route path='orders' element={<ProfileOrders />} />
+              </Route>
+              <Route element={<ProtectedRoute onlyUnAuth />}>
+                <Route path='login' element={<Login />} />
+                <Route path='register' element={<Register />} />
+                <Route path='forgot-password' element={<ForgotPassword />} />
+                <Route path='reset-password' element={<ResetPassword />} />
+              </Route>
+            </Route>
+          </Routes>
+
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={
+                <Modal title={''} onClose={() => navigate(-1)}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+            <Route element={<ProtectedRoute />}>
+              <Route
+                path='/profile/orders/:number'
+                element={
+                  <Modal title={''} onClose={() => navigate(-1)}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+            </Route>
+          </Routes>
+        </>
       )}
     </div>
   );
