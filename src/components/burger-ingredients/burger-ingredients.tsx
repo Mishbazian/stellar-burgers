b05a7bd6615f522356bedf841401598a6db9fb43
Mikhail Ivanov <shirokopuh@gmail.com>
2026-04-20 17:02:42 +0700

    refactor(slices): add index file to export slices

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import { TTabMode } from '@utils-types';
 import { BurgerIngredientsUI } from '../ui/burger-ingredients';
 import { useSelector } from 'react-redux';
-import { getGrouppedIngredients } from '../../services/slices/ingredients-slice';
+import { getGrouppedIngredients } from '@slices';
 
 export const BurgerIngredients: FC = () => {
   const { bun, main, sauce } = useSelector(getGrouppedIngredients);
