a8532d573466b79867019da13e13fafc10543c63
Mikhail Ivanov <shirokopuh@gmail.com>
2026-04-22 00:45:14 +0700

    feat(protected-route): connect store to protected route

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,17 +1,18 @@
 import { Navigate } from 'react-router';
 import { Outlet } from 'react-router-dom';
+import { getUserSelector, useSelector } from '../../services';
 
 type ProtectedRouteProps = {
   onlyUnAuth?: boolean;
 };
 
 export const ProtectedRoute = ({ onlyUnAuth = false }: ProtectedRouteProps) => {
-  //TODO добавить данные из store
-  const isAuthenticated = false;
-  //TODO обновить маршруты по ТЗ
-  if (onlyUnAuth) {
-    return isAuthenticated ? <Navigate to='/' replace /> : <Outlet />;
-  }
+  const { user } = useSelector(getUserSelector);
 
-  return isAuthenticated ? <Outlet /> : <Navigate to='/login' replace />;
+  if (onlyUnAuth && user) {
+    return <Navigate to='/' replace />;
+  }
+  if (!user && !onlyUnAuth) return <Navigate to='/login' />;
+
+  return <Outlet />;
 };
