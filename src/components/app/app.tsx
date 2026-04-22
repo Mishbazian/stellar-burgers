6a17c79e0f39d4fab6f3f6b9e0b24b54c565674a
Mikhail Ivanov <shirokopuh@gmail.com>
2026-04-23 04:31:33 +0700

    refactor(app routes): rebuild routes with new components

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,28 +7,29 @@ import {
   Profile,
   ProfileOrders,
   Register,
-  ResetPassword
+  ResetPassword,
+  DetailPage
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { ProtectedRoute } from '../protected-route';
-import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { IngredientDetails, Modal, OrderInfo } from '@components';
 import { TITLES } from '../../utils/constants';
 
 import { useEffect } from 'react';
 import { getIngredients, getUser, getUserSelector } from '@slices';
 import { useDispatch, useSelector } from '../../services';
 import { getCookie } from '../../utils/cookie';
+import AppLayout from '../../layouts/app-layout';
 
-export const App = () => {
+const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const backgroundLocation = location.state?.background;
 
   const dispatch = useDispatch();
   //TODO пересмотреть получение номера заказа для заголовка модалки
-  //TODO вынести обертку для немодальных страниц в отдельный HOC
   //TODO переписать классы через clsx?
   //TODO найти причину ререндера App
   const lastUripart: string = location.pathname.split('/').pop() ?? '';
@@ -39,72 +40,90 @@ export const App = () => {
     if (getCookie('accessToken') && !isInit) dispatch(getUser());
   }, []);
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      <>
-        <Routes location={backgroundLocation || location}>
-          <Route path='/'>
-            <Route index element={<ConstructorPage />} />
-            <Route path='feed' element={<Feed />} />
-            <Route path='profile' element={<ProtectedRoute />}>
-              <Route index element={<Profile />} />
-              <Route path='orders' element={<ProfileOrders />} />
-            </Route>
-            <Route element={<ProtectedRoute onlyUnAuth />}>
-              <Route path='login' element={<Login />} />
-              <Route path='register' element={<Register />} />
-              <Route path='forgot-password' element={<ForgotPassword />} />
-              <Route path='reset-password' element={<ResetPassword />} />
-            </Route>
+    <>
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<ConstructorPage />} />
+          <Route path='feed' element={<Feed />} />
+          <Route
+            path='ingredients/:id'
+            element={
+              <DetailPage title={TITLES.INGREDIENTS_DETAILS}>
+                <IngredientDetails />
+              </DetailPage>
+            }
+          />
+          <Route
+            path='/feed/:number'
+            element={
+              <DetailPage title={`#${lastUripart}`}>
+                <OrderInfo />
+              </DetailPage>
+            }
+          />
+
+          <Route path='profile' element={<ProtectedRoute />}>
+            <Route index element={<Profile />} />
+            <Route path='orders' element={<ProfileOrders />} />
             <Route
-              path='/feed/:number'
+              path='orders/:number'
               element={
-                <div className={styles.detailPageWrap}>
-                  <h2
-                    className={`${styles.detailHeader} text text_type_main-large`}
-                  >{`#${lastUripart}`}</h2>
+                <DetailPage title={`#${lastUripart}`}>
                   <OrderInfo />
-                </div>
+                </DetailPage>
               }
             />
           </Route>
+          <Route element={<ProtectedRoute onlyUnAuth />}>
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='forgot-password' element={<ForgotPassword />} />
+            <Route path='reset-password' element={<ResetPassword />} />
+          </Route>
           <Route path='*' element={<NotFound404 />} />
-        </Routes>
-        {backgroundLocation && (
-          <Routes>
+        </Route>
+      </Routes>
+
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal
+                title={`#${lastUripart}`}
+                onClose={() => navigate(backgroundLocation)}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='ingredients/:id'
+            element={
+              <Modal
+                title={TITLES.INGREDIENTS_DETAILS}
+                onClose={() => navigate(backgroundLocation)}
+              >
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route element={<ProtectedRoute />}>
             <Route
-              path='/feed/:number'
+              path='/profile/orders/:number'
               element={
-                <Modal title={`#${lastUripart}`} onClose={() => navigate(-1)}>
+                <Modal
+                  title={`#${lastUripart}`}
+                  onClose={() => navigate(backgroundLocation)}
+                >
                   <OrderInfo />
                 </Modal>
               }
             />
-            <Route
-              path='/ingredients/:id'
-              element={
-                <Modal
-                  title={TITLES.INGREDIENTS_DETAILS}
-                  onClose={() => navigate(-1)}
-                >
-                  <IngredientDetails />
-                </Modal>
-              }
-            />
-            <Route element={<ProtectedRoute />}>
-              <Route
-                path='/profile/orders/:number'
-                element={
-                  <Modal title={''} onClose={() => navigate(-1)}>
-                    <OrderInfo />
-                  </Modal>
-                }
-              />
-            </Route>
-          </Routes>
-        )}
-      </>
-    </div>
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 };
 
