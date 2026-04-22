0344238d4e481fa1f05fffb6f7d994b24e0b5022
Mikhail Ivanov <shirokopuh@gmail.com>
2026-04-23 04:34:12 +0700

    feat(header-menu): add styles of active link

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -14,16 +14,28 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
         <>
-          <BurgerIcon type={'primary'} />
-          <NavLink to={''}>
+          <NavLink
+            to={'/'}
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
+          >
+            <BurgerIcon type={'primary'} />
+
             <p className='text text_type_main-default ml-2 mr-10'>
               Конструктор
             </p>
           </NavLink>
         </>
         <>
-          <ListIcon type={'primary'} />
-          <NavLink to={'feed'}>
+          <NavLink
+            to={'feed'}
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
+          >
+            <ListIcon type={'primary'} />
+
             <p className='text text_type_main-default ml-2'>Лента заказов</p>
           </NavLink>
         </>
@@ -33,8 +45,14 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
       </div>
 
       <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />{' '}
-        <NavLink to={'profile'}>
+        {' '}
+        <NavLink
+          to={'profile'}
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
+          <ProfileIcon type={'primary'} />{' '}
           <p className='text text_type_main-default ml-2'>
             {userName || 'Личный кабинет'}
           </p>
