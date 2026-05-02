32dd69fe93d75e7a84e3ec6b5f04e0e1a4249bc3
Mikhail Ivanov <shirokopuh@gmail.com>
2026-05-02 20:43:35 +0700

    build(test): configure jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '@pages': '<rootDir>/src/pages',
+    '@components': '<rootDir>/src/components',
+    '@ui': '<rootDir>/src/components/ui',
+    '@ui-pages': '<rootDir>/src/components/ui/pages',
+    '@utils-types': '<rootDir>/src/utils/types',
+    '@api': '<rootDir>/src/utils/burger-api.ts',
+    '@slices': '<rootDir>/src/services/slices',
+    '@selectors': '<rootDir>/src/services/selectors'
+  }
+};
+export default config;
