9f80bd7ec374ecccd7c7d48bc43d9bc2420fff72
Mikhail Ivanov <shirokopuh@gmail.com>
2026-05-05 12:21:54 +0700

    test(ingredients-slice): refactor ingredients-slice.spec with new util function

--- src/services/slices/ingredients-slice/ingredients-slice.spec.ts
+++ src/services/slices/ingredients-slice/ingredients-slice.spec.ts
@@ -1,75 +1,52 @@
-import { configureStore } from '@reduxjs/toolkit';
 import {
   getIngredients,
   ingredientsSlice,
   initialIngredientsState
 } from './ingredients-slice';
-import * as api from '@api';
 import { ingredients, ingredientsResponse } from '@test-data';
-import { expect, it, describe, jest } from '@jest/globals';
+import { describe, jest } from '@jest/globals';
+import { setAsyncActionTests } from '@test-utils';
 
 describe('тест ingredients-slice', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-    jest.clearAllTimers();
-  });
-  describe('[#1] asyncThunk getIngredients корректно работает с хранилищем', () => {
-    it('[#1.1] getIngredients.pending устанавливает isLoading', async () => {
-      const getIngredientsApiSpy = jest.spyOn(api, 'getIngredientsApi');
-      //Подменяем запрос Промисом с таймаутом
-      jest
-        .spyOn(api, 'getIngredientsApi')
-        .mockImplementation(
-          () => new Promise((resolve) => setTimeout(resolve, 100000))
-        );
-
-      const store = configureStore({
-        reducer: { ingredients: ingredientsSlice.reducer }
+  describe('asyncThunk getIngredients корректно работает с хранилищем', () => {
+    const { testCaseFulfilled, testCasePending, testCaseRejected } =
+      setAsyncActionTests({
+        slice: ingredientsSlice,
+        action: getIngredients,
+        initialState: initialIngredientsState,
+        spyFn: 'getIngredientsApi'
       });
-      const promise = store.dispatch(getIngredients());
-      const expectedState = {
-        ...initialIngredientsState,
-        isLoading: true
-      };
 
-      expect(getIngredientsApiSpy).toHaveBeenCalledTimes(1);
-      expect(store.getState().ingredients).toEqual(expectedState);
-      //Принудительно отменяем промис
-      promise.abort();
-    });
-    it('[#1.1] getIngredients.fullfilled сохраняет корректные данные и сбрасывает isLoading', async () => {
-      const getIngredientsApiSpy = jest
-        .spyOn(api, 'getIngredientsApi')
-        .mockResolvedValue(ingredientsResponse.data);
-      const store = configureStore({
-        reducer: { ingredients: ingredientsSlice.reducer }
-      });
-      await store.dispatch(getIngredients());
-
-      const expectedState = {
-        ...initialIngredientsState,
-        isLoading: false,
-        ingredients: ingredients
-      };
-      expect(getIngredientsApiSpy).toHaveBeenCalledTimes(1);
-      expect(store.getState().ingredients).toEqual(expectedState);
-    });
-    it('[#1.2] getIngredients.rejected сохраняет сообщение ошибки и сбрасывает isLoading', async () => {
-      const errorMessage = 'Текст ошибки';
-      const getIngredientsApiSpy = jest
-        .spyOn(api, 'getIngredientsApi')
-        .mockRejectedValue({ message: errorMessage });
-      const store = configureStore({
-        reducer: { ingredients: ingredientsSlice.reducer }
-      });
-      await store.dispatch(getIngredients());
-      const expectedState = {
-        ...initialIngredientsState,
-        isLoading: false,
-        error: errorMessage
-      };
-      expect(getIngredientsApiSpy).toHaveBeenCalledTimes(1);
-      expect(store.getState().ingredients).toEqual(expectedState);
-    });
+    it(
+      '[#1.1] getIngredients.pending устанавливает isLoading',
+      testCasePending({
+        expectedState: {
+          ...initialIngredientsState,
+          isLoading: true
+        }
+      })
+    );
+    it(
+      '[#1.2] getIngredients.fullfilled сохраняет корректные данные и сбрасывает isLoading',
+      testCaseFulfilled({
+        mockResolvedValue: ingredientsResponse.data,
+        expectedState: {
+          ...initialIngredientsState,
+          isLoading: false,
+          ingredients: ingredients
+        }
+      })
+    );
+    it(
+      '[#1.3] getIngredients.rejected сохраняет сообщение ошибки и сбрасывает isLoading',
+      testCaseRejected({
+        mockRejectedValue: { message: 'Error message' },
+        expectedState: {
+          ...initialIngredientsState,
+          isLoading: false,
+          error: 'Error message'
+        }
+      })
+    );
   });
 });
